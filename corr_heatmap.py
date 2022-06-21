c994afb2433e44b06370c62674dad39ff3b8fbfd
Nojam <48527007+loriza@users.noreply.github.com>
2022-06-21 16:02:52 +0800

    Update corr_heatmap.py

--- corr_heatmap.py
+++ corr_heatmap.py
@@ -13,8 +13,8 @@ import argparse
 
 class DESCRIPTION:
     Program = "Program: %(prog)s (Draw corplots via Seaborn)"
-    Version = "Version: 2022.05.07-15.40"
-    Description = "%s\n%s\n%s" % (Program, Version, Contact)
+  
+    Description = "%s\n%s\n%s" % (Program)
 
 
 USER = getpass.getuser()
@@ -25,61 +25,80 @@ mpl.use('Agg')
 mpl.rcParams['font.family'] = 'Microsoft YaHei'
 mpl.rcParams['font.sans-serif'] = ['Microsoft YaHei']
 
-#mpl.rc("font",family='Microsoft YaHei')
+mpl.rc("font",family='Microsoft YaHei')
 
-# 设置文件夹的路径
-curpath = os.path.dirname(os.path.realpath(__file__))
-path1 = os.path.join(curpath, 'heatmap_plot.py')
-# 改变路径和打印路径的信息
-warnings.filterwarnings("ignore")
 
 delimiter = '\t'
 
 
 def df_reader(path):
     try:
-        return pd.read_table(path, sep=delimiter, comment='#', index_col=0, header=0)
+        df = pd.read_table(path, sep=delimiter, comment='#', index_col=0, header=0)
+        d1 = df.dropna(axis=1, how='all', thresh=None, subset=None, inplace=False)
+        return d1.dropna(axis=0, how='all', thresh=None, subset=None, inplace=False)
+        
     except Exception:
         sys.exit(f"{path} does not exist")
 
 
-def sub_df(df, min_r2, df2, filter):
-    if filter is True:
-        #去除所有r都小于cutoff的行/列
-        drop_row = df[(df.abs() < min_r2).all(axis=1)].index.values.tolist()
-        drop_col = df[df.columns[(df.abs() < min_r2).all(axis=0)]].columns.values.tolist()
-
-        df = df.drop(drop_row, inplace=False).drop(drop_col, inplace=False, axis=1)
-        df2 = df2.drop(drop_row, inplace=False).drop(drop_col, inplace=False, axis=1)
-
-    #去除含有na的行/列
-    new_df = df.dropna(axis=1, how='all', inplace=False).dropna(axis=0, how='all', inplace=False)
-    new_df2 = df2.dropna(axis=1, how='all', inplace=False).dropna(axis=0, how='all', inplace=False)
+def sub_df(coef, min_r2, pvals, filter, filter_ns):
     
-    return new_df, new_df2
+    """去除元素全为na的行/列"""
+    coef = coef.dropna(axis=1, how='all', inplace=False).dropna(axis=0, how='all', inplace=False)
+    pvals = pvals.dropna(axis=1, how='all', inplace=False).dropna(axis=0, how='all', inplace=False)
+    if filter is True:
+        """去除所有r都小于cutoff的行/列"""
+        print("filtration based on rcutoff now starting")
+        drop_row = coef[(coef.abs() < min_r2).all(axis=1)].index.values.tolist()
+        drop_col = coef[coef.columns[(coef.abs() < min_r2).all(axis=0)]].columns.values.tolist()
+
+        coef = coef.drop(drop_row, inplace=False).drop(drop_col, inplace=False, axis=1)
+        pvals = pvals.drop(drop_row, inplace=False).drop(drop_col, inplace=False, axis=1)
+
+    if filter_ns is True:
+        print("filtration based on pcutoff now starting")
+        coef, pvals = filter_out_ns(coef, pvals, 0.05)
+
+
+    return coef, pvals
+
+def filter_out_ns(coef, pvals, max_pval):
+    #perform on pvalue dataframe
+    """去除只包含NaN和NS的行/列"""
+    pvals[np.isnan(pvals)] = 1    
+
+    drop_row = pvals[(pvals.abs() > max_pval).all(axis=1)].index.values.tolist()
+    drop_col = pvals[pvals.columns[(pvals.abs() > max_pval).all(axis=0)]].columns.values.tolist()
+    pvals = pvals.drop(drop_row, inplace=False).drop(drop_col, inplace=False, axis=1)
+    coef = coef.drop(drop_row, inplace=False).drop(drop_col, inplace=False, axis=1)
+
+    return coef, pvals
 
 def flatten(pval):
     mydata = pval.copy()
-
+    
     mask1 = np.array(mydata)
     mask2 = mask1.flatten()
-    mask3 = [str(x) for x in mask2]
+    
+    mask3 = [float(x) for x in mask2]
+    print(mask3)
     for i in range(len(mask3)):
-        if mask3[i] > str(0.05):
+        if mask3[i] > 0.05:
             mask3[i] = " "
-        elif (mask3[i] <= str(0.05)) & ((mask3[i]) > str(0.01)):
+        elif (mask3[i] <= 0.05) & ((mask3[i]) > 0.01):
             mask3[i] = "*"
-        elif mask3[i] <= str(0.01):
+        elif (mask3[i] <= 0.01) & ((mask3[i]) > 0.001):
             mask3[i] = "**"
-        elif mask3[i] <= str(0.001):
+        elif mask3[i] <= 0.001:
             mask3[i] = "***"
-    mask4 = mask3
-    # print(mask4)
-    return mask4
+        elif np.isnan(mask3[i]): mask3[i] = " "
+    print(mask3)
+    return mask3
 
 def sumn(data, pvalue):
+
     mask = flatten(pvalue)
-    print(len(mask))
+
     if (data.shape[0]+data.shape[1]) < 100:
         label_size = 12
         tick_size = 12
@@ -176,10 +195,9 @@ def corr_heatmap(corr, prefix, height, width, fmt, methods):
 def cluster_heatmap(corr, prefix, width, height, fmt, methods, clusterlist):
 
     fig, ax2 = plt.subplots()
-
     sns.set(font_scale=1, style="dark")
     sns.set_context("paper")
-    ax2 = sns.clustermap(data=corr,
+    ax2 = sns.clustermap(data=corr.isnull(),
                          figsize=(width, height),
                          vmin=-1,
                          vmax=1,
@@ -214,7 +232,9 @@ def cluster_heatmap(corr, prefix, width, height, fmt, methods, clusterlist):
     plt.setp(ax2.ax_heatmap.get_yticklabels(), fontsize=ticks_size)
 
     plt.tight_layout()
+ 
     outfile = prefix + '.cluster.heatmap.' + fmt
+
     plt.savefig(outfile, bbox_inches='tight',dpi=dpi)
     plt.close()
     print(f"Cluster heatmap: {outfile}")
@@ -229,7 +249,7 @@ if __name__ == '__main__':
                         help='input pvalues data file')
     parser.add_argument("-o", dest="output",
                         help="directory and prefix of out images")
-    parser.add_argument('-f', dest="format", default='pdf', nargs='*',
+    parser.add_argument('-f', dest="format", default='jpg', type=str,
                         choices=['png', 'pdf', 'svg', 'jpg'],
                         help='output heatmap image format: default jpg')
     parser.add_argument('-t', dest="methods", default='spearman',
@@ -245,8 +265,9 @@ if __name__ == '__main__':
     parser.add_argument('-w', dest="width", default=22, type=float,
                         help='image height, default 22 inches')
     parser.add_argument("-m", "--verbose", action="store_false",
-                        help=" if given, filtering based on the r cutoff will not be performed")
-
+                        help=" if given, filtering based on r cutoff will not be performed")
+    parser.add_argument("-n", "--nons", action="store_true",
+                        help=" if given, filtering based on p cutoff will be performed")
     args = parser.parse_args()
 
     if os.path.exists(os.path.dirname(args.output)) is False:
@@ -256,9 +277,14 @@ if __name__ == '__main__':
     coef = df_reader(args.c)
     pvalues = df_reader(args.p)
 
-    coef, pvalues = sub_df(coef, args.rc, pvalues, args.verbose)
-    method = legend_title(args.methods)
-    lab_size, ticks_size, dpi, annot, lw = sumn(coef, pvalues)
+    coef, pvalues = sub_df(coef, args.rc, pvalues, args.verbose,args.nons)
+
+    #coef = coef.replace(np.nan, 0)
+    #print(coef)
     
+    method = legend_title(args.methods)
+    print(pvalues)
+    lab_size, ticks_size, dpi, annot, lw = sumn(coef, pvalues)
+
     cluster_heatmap(coef, args.output, args.width, args.height, args.format, method, args.list1)
     corr_heatmap(coef, args.output, args.height, args.width, args.format, method)
